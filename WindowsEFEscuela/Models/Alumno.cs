92944b4bd5b7cfe234fa44aa63f37aecb3cba40e
Ariana Belgrado <belgradoariana@gmail.com>
2023-01-19 20:35:39 -0300

    Agregar archivos de proyecto.

---
+++ WindowsEFEscuela/Models/Alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsEFEscuela.Models
+{
+    internal class Alumno
+    {
+        public int AlumnoId { get; set; }
+        public string Name { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
